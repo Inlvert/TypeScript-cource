b1ea2ce46dc710dc551ee632b4f923fd1a59980e
Boris Ushakov <inlvert@gmail.com>
2025-05-22 13:56:04 +0300

    add task - conditions + solution

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
-var num = 300;
-console.log(num);
+// let userName: string = "Boris";
+// userName.length;
+// console.log(userName.length);
+// const isBirthday: boolean = true;
+// let age: number = 40;
+// const userName: string = 'John';
+// if(isBirthday) {
+//   console.log(`congrats ${userName.toUpperCase()}, age${age + 1}`)
+// }
+//------------------- function
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
+// function foo(isBirthday: boolean, age: number, userName: string): string {
+//   if (isBirthday) {
+//     return `congrats ${userName.toUpperCase()}, age${age + 1}`;
+//   }
+//   return 'warning'
+// }
+//----------------------------------
+// const foo = (isBirthday: boolean, age: number, userName: string): string => {
+//   if (isBirthday) {
+//     return `congrats ${userName.toUpperCase()}, age${age + 1}`;
+//   }
+//   return "warning";
+// };
+// foo(isBirthdayData, 34, "Boris");
+//-------------------- type any
+// const userData =
+//   '{"isBirthdayData": "true", "ageData": 34, "userNameData": "John"}';
+// const userObj: {
+//   isBirthdayData: boolean;
+//   ageData: number;
+//   userNameData: string;
+// } = JSON.parse(userData);
+// console.log(userObj)
+// const foo = (isBirthday: boolean, age: number, userName: string): string => {
+//   if (isBirthday) {
+//     return `congrats ${userName.toUpperCase()}, age${age + 1}`;
+//   }
+//   return "warning";
+// };
+// foo(userObj.isBirthdayData, 34, "Boris");
+//------------------------ task
+var currRate = "1.05";
+var fetchCurr = function (response) {
+    var data = JSON.parse(response);
+    return data;
+};
+function transferEurToUsd(available, amount, commission) {
+    if (available) {
+        var res = +fetchCurr(currRate) * amount * commission;
+        console.log(res);
+    }
+    else {
+        console.log('you can not exchange Eur');
+    }
+}
+transferEurToUsd(true, 1000, 1.05);
--- index.ts
+++ index.ts
@@ -14,9 +14,9 @@
 
 //------------------- function
 
-const isBirthdayDate: boolean = true;
-let ageDate: number = 40;
-const userNameDate: string = "John";
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
 
 // function foo(isBirthday: boolean, age: number, userName: string): string {
 //   if (isBirthday) {
@@ -26,16 +26,78 @@ const userNameDate: string = "John";
 //   return 'warning'
 // }
 
-const foo = (isBirthday: boolean, age: number, userName: string): string => {
-  if (isBirthday) {
-    return `congrats ${userName.toUpperCase()}, age${age + 1}`;
-  }
+//----------------------------------
 
-  return "warning";
-};
+// const foo = (isBirthday: boolean, age: number, userName: string): string => {
+//   if (isBirthday) {
+//     return `congrats ${userName.toUpperCase()}, age${age + 1}`;
+//   }
 
-foo(isBirthdayDate, 34, "Boris");
+//   return "warning";
+// };
+
+// foo(isBirthdayData, 34, "Boris");
 
 //-------------------- type any
 
+// const userData =
+//   '{"isBirthdayData": "true", "ageData": 34, "userNameData": "John"}';
 
+// const userObj: {
+//   isBirthdayData: boolean;
+//   ageData: number;
+//   userNameData: string;
+// } = JSON.parse(userData);
+
+// console.log(userObj)
+
+// const foo = (isBirthday: boolean, age: number, userName: string): string => {
+//   if (isBirthday) {
+//     return `congrats ${userName.toUpperCase()}, age${age + 1}`;
+//   }
+
+//   return "warning";
+// };
+
+// foo(userObj.isBirthdayData, 34, "Boris");
+
+
+//------------------------ task - conditions
+
+// const currRate = "1.05";
+
+// const fetchCurr = (response) => {
+//   const data = JSON.parse(response);
+//   return data;
+// }
+
+// function transferEurToUsd(available, amount, commission) {
+//   if(available) {
+//     let res = fetchCurr(currRate) * amount * commission;
+//     console.log(res);
+//   } else {
+//     console.log('you can not exchange Eur')
+//   }
+// }
+
+// transferEurToUsd(true, 500, 1.05)
+
+//------------------------- task - solution
+
+const currRate = "1.05";
+
+const fetchCurr = (response: string): string => {
+  const data = JSON.parse(response);
+  return data;
+}
+
+function transferEurToUsd(available: boolean, amount: number, commission: number) {
+  if(available) {
+    let res = +fetchCurr(currRate) * amount * commission;
+    console.log(res);
+  } else {
+    console.log('you can not exchange Eur')
+  }
+}
+
+transferEurToUsd(true, 1000, 1.05)
