b6aed068729b87fc30fc31ba8e15eb6317e6478f
Boris Ushakov <inlvert@gmail.com>
2025-05-23 14:24:34 +0300

    add tuples

--- index.ts
+++ index.ts
@@ -188,41 +188,174 @@
 
 //---------------------------- destructuring
 
-const isBirthdayData: boolean = true;
-let ageData: number = 34;
-const userNameData: string = "John";
+// const isBirthdayData: boolean = true;
+// let ageData: number = 34;
+// const userNameData: string = "John";
 
-const userData = {
-  isBirthdayData: true,
-  ageData: 34,
-  userNameData: "John",
-  messages: {
-    error: "Error",
-  },
-};
+// const userData = {
+//   isBirthdayData: true,
+//   ageData: 34,
+//   userNameData: "John",
+//   messages: {
+//     error: "Error",
+//   },
+// };
 
-const createError = (mag: string) => {
-  throw new Error("Error");
-};
+// const createError = (mag: string) => {
+//   throw new Error("Error happend");
+// };
 
-function foo({
-  isBirthdayData: isBirth, // destructuring and rename
-  ageData: age, // destructuring and rename
-  userNameData: user, // destructuring and rename
-  messages: { error }, // destructuring
-}: {
-  isBirthdayData: boolean;
-  ageData: number;
-  userNameData: string;
-  messages: {
-    error: string;
-  };
-}): string {
-  if (isBirth === true) {
-    return `congrats ${user.toUpperCase()}, age${age + 1}`;
-  } else {
-    return createError(error);
-  }
-}
+// function foo({
+//   isBirthdayData: isBirth, // destructuring and rename
+//   ageData: age, // destructuring and rename
+//   userNameData: user, // destructuring and rename
+//   messages: { error }, // destructuring
+// }: {
+//   isBirthdayData: boolean;
+//   ageData: number;
+//   userNameData: string;
+//   messages: {
+//     error: string;
+//   };
+// }): string {
+//   if (isBirth === true) {
+//     return `congrats ${user.toUpperCase()}, age${age + 1}`;
+//   } else {
+//     return createError(error);
+//   }
+// }
 
-console.log(foo(userData));
+// console.log(foo(userData));
+
+//------------------------ Array
+
+// const departments: string[] = ["dev", "desing", "marketing"];
+
+// const nums: number[][] = [[1, 2, 3], [4, 5, 6]];
+
+// const department1 = departments[0];
+
+// departments.push('')
+
+// const report = departments.filter((d: string) => d !== "dev").map((d: string) => `${d} - done`);
+
+// const [first, second] = report //destructuring array
+
+// console.log(first)
+// console.log(second)
+
+//------------------------ task V1
+
+// const electricityUserData = {
+//   readings: 95,
+//   units: "kWt",
+//   mode: "double",
+// };
+
+// const waterUserData = {
+//   readings: 3,
+//   units: "m3",
+// };
+
+// const elRate: number = 0.45;
+// const wRate: number = 2;
+
+// const monthPayments: number[] = [0, 0]; // [electricity, water]
+// // elData: { readings: number; mode: string; units: string },
+// // wData: { readings: number; units: string },
+
+// const calculatePayments = (
+//   { readings, mode }: { readings: number; mode: string },
+//   wData: { readings: number },
+//   elRate: number,
+//   wRate: number
+// ): void => {
+//   if (mode === "double" && readings < 50) {
+//     monthPayments[0] = readings * elRate * 0.7;
+//   } else {
+//     monthPayments[0] = readings * elRate;
+//   }
+
+//   monthPayments[1] = wData.readings * wRate;
+// };
+
+// calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+
+// const sendInvoice = (
+//   [el, water]: number[],
+//   electricityUserData: { readings: number; mode: string; units: string },
+//   waterUserData: { readings: number; units: string }
+// ): string => {
+//   const text = `    Hello!
+//     This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+//     It will cost: ${monthPayments[0]}€
+    
+//     This month you used ${waterUserData.readings} ${waterUserData.units} of water
+//     It will cost: ${monthPayments[1]}€`;
+
+//   return text;
+// };
+
+// console.log(sendInvoice(monthPayments, electricityUserData, waterUserData));
+
+// //------------------------task V2
+
+// const electricityUserData = {
+// 	readings: 95,
+// 	units: "kWt",
+// 	mode: "double",
+// };
+
+// const waterUserData = {
+// 	readings: 3,
+// 	units: "m3",
+// };
+
+// const elRate: number = 0.45;
+// const wRate: number = 2;
+
+// const monthPayments: number[] = [0, 0]; // [electricity, water]
+
+// const calculatePayments = (
+// 	{ readings, mode }: { readings: number; mode: string },
+// 	wData: { readings: number },
+// 	elRate: number,
+// 	wRate: number
+// ): void => {
+// 	if (mode === "double" && readings < 50) {
+// 		monthPayments[0] = readings * elRate * 0.7;
+// 	} else {
+// 		monthPayments[0] = readings * elRate;
+// 	}
+
+// 	monthPayments[1] = wData.readings * wRate;
+// };
+
+// calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+
+// const sendInvoice = (
+// 	[el, water]: number[],
+// 	electricityUserData: { readings: number; units: string },
+// 	waterUserData: { readings: number; units: string }
+// ): string => {
+// 	const text = `    Hello!
+//     This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+//     It will cost: ${el}€
+    
+//     This month you used ${waterUserData.readings} ${waterUserData.units} of water
+//     It will cost: ${water}€`;
+
+// 	return text;
+// };
+
+// const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
+// console.log(invoice);
+
+//-------------------------------- Tuples Кортеж
+
+const UserDataTuple: [boolean, number, string] = [true, 40, "John"];
+
+const UserDataTuple2: [boolean, number, ...string[]] = [true, 40, "John", "Boris", "Yehor", ];
+
+const UserDataTuple3: [...string[], boolean, number] = ["John", "Boris", "Yehor", true, 40, ];
+
